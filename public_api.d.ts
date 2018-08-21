86807469ae201843d79c0ade647bcde92ac33cb8
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-08-21 23:36:24 +0200

    change

--- public_api.d.ts
+++ public_api.d.ts
@@ -1,4 +1,6 @@
+export { Field } from './lib/base/field';
+export { FieldConfig } from './lib/base/field.config';
 export { MyButtonComponent } from './lib/my-button/my-button.component';
 export { MyInputComponent } from './lib/my-input/my-input.component';
-export * from './lib/my-select/my-select.component';
+export { MySelectComponent } from './lib/my-select/my-select.component';
 export * from './lib/ui-components.module';
