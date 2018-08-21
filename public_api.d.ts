e415a5f5f383226ae8391351d0682c17438597b7
lukaskellerstein <lukas.kellerstein@gmail.com>
2018-08-21 22:08:57 +0200

    test

--- public_api.d.ts
+++ public_api.d.ts
@@ -1,3 +1,5 @@
 export * from './lib/base/field';
 export * from './lib/base/field.config';
+export * from './lib/my-button/my-button.component';
+export * from './lib/my-input/my-input.component';
 export * from './lib/ui-components.module';
--- ui-components.d.ts
+++ ui-components.d.ts
@@ -2,6 +2,4 @@
  * Generated bundle index. Do not edit.
  */
 export * from './public_api';
-export { MyButtonComponent as ɵb } from './lib/my-button/my-button.component';
-export { MyInputComponent as ɵa } from './lib/my-input/my-input.component';
-export { MySelectComponent as ɵc } from './lib/my-select/my-select.component';
+export { MySelectComponent as ɵa } from './lib/my-select/my-select.component';
